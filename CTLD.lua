960808e52e54ab016a0744b2a3eb0b7f28d52e53
Grizlog <3rdwood@gmail.com>
2021-10-01 19:34:22 -0500

    broke extract distance

--- CTLD.lua
+++ CTLD.lua
@@ -52,11 +52,7 @@ ctld.slingLoad = false -- if false, crates can be used WITHOUT slingloading, by 
 
 ctld.enableSmokeDrop = true -- if false, helis and c-130 will not be able to drop smoke
 
-<<<<<<< HEAD
-ctld.maxExtractDistance = 250 -- max distance from vehicle to troops to allow a group extraction
-=======
 ctld.maxExtractDistance = 500 -- max distance from vehicle to troops to allow a group extraction
->>>>>>> be671c47137a577171f2af389b3d64c62fa07aba
 ctld.maximumDistanceLogistic = 200 -- max distance from vehicle to logistics to allow a loading or spawning operation
 ctld.maximumSearchDistance = 4000 -- max distance for troops to search for enemy
 ctld.maximumMoveDistance = 2000 -- max distance for troops to move from drop point if no enemy is nearby
