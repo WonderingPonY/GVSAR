e075fd8e0b285552f546dfa72d0e4fd3b0451e34
Grizlog <3rdwood@gmail.com>
2021-10-17 19:55:57 -0500

    added central and southern border tasking

--- Border Tasking.lua
+++ Border Tasking.lua
@@ -14,4 +14,26 @@ AttackGroups = SET_GROUP:New():FilterCoalitions( "blue" ):FilterStart()
 
 TargetSetUnit = SET_UNIT:New():FilterCoalitions("red"):FilterPrefixes( "Northern" ):FilterStart()
 
-TaskBAI = TASK_A2G_BAI:New( Northern_Border_Push, AttackGroups,"Northern Border sweep", TargetSetUnit )
+TaskCAS = TASK_A2G_CAS:New( Northern_Border_Push, AttackGroups,"Northern Border sweep", TargetSetUnit )
+
+--Central Border Task
+Central_Border_Push = MISSION
+  :New( CommandCenter, "Central Border Strike", "Primary", "Sweep the boarder and eliminate enenmy forces!", coalition.side.blue )
+  :AddScoring( Scoring )
+
+AttackGroups = SET_GROUP:New():FilterCoalitions( "blue" ):FilterStart()
+
+TargetSetUnit = SET_UNIT:New():FilterCoalitions("red"):FilterPrefixes( "Central" ):FilterStart()
+
+TaskCAS = TASK_A2G_CAS:New( Central_Border_Push, AttackGroups,"Central Border sweep", TargetSetUnit )
+
+--Southern Border Task
+Southern_Border_Push = MISSION
+  :New( CommandCenter, "Southern Border Strike", "Primary", "Sweep the boarder and eliminate enenmy forces!", coalition.side.blue )
+  :AddScoring( Scoring )
+
+AttackGroups = SET_GROUP:New():FilterCoalitions( "blue" ):FilterStart()
+
+TargetSetUnit = SET_UNIT:New():FilterCoalitions("red"):FilterPrefixes( "Southern" ):FilterStart()
+
+TaskCAS = TASK_A2G_CAS:New( Southern_Border_Push, AttackGroups,"Southern Border sweep", TargetSetUnit )
