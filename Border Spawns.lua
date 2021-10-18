9961e543168639a1711db4022830b0bc4ad3c466
Grizlog <3rdwood@gmail.com>
2021-10-17 20:14:28 -0500

    added central spawn forces/zones

--- Border Spawns.lua
+++ Border Spawns.lua
@@ -1,4 +1,4 @@
---Northern Spawns
+--Northern Spawns Zones
 NorhternSpawnZones = {
 ZONE:New( "Northern Border Spawn Zone-1" ),
 ZONE:New( "Northern Border Spawn Zone-2" ),
@@ -14,11 +14,35 @@ ZONE:New( "Northern Border Spawn Zone-11" ),
 ZONE:New( "Northern Border Spawn Zone-12" ),
 ZONE:New( "Northern Border Spawn Zone-13" ) }
 
---Northern unit templates
+--Central Spawn Zones
+CentralSpawnZones = {
+ZONE:New( "Central Border Spawn Zone-1" ),
+ZONE:New( "Central Border Spawn Zone-2" ),
+ZONE:New( "Central Border Spawn Zone-3" ),
+ZONE:New( "Central Border Spawn Zone-4" ),
+ZONE:New( "Central Border Spawn Zone-5" ),
+ZONE:New( "Central Border Spawn Zone-6" ),
+ZONE:New( "Central Border Spawn Zone-7" ),
+ZONE:New( "Central Border Spawn Zone-8" ),
+ZONE:New( "Central Border Spawn Zone-9" ),
+ZONE:New( "Central Border Spawn Zone-10" ),
+ZONE:New( "Central Border Spawn Zone-11" ),
+ZONE:New( "Central Border Spawn Zone-12" ),
+ZONE:New( "Central Border Spawn Zone-13" ) }
+
+
+--Northern Unit Templates
 NBU = {
 "Northern Border Units-1",
 "Northern Border Units-2",
 "Northern Border Units-3" }
+
+--Central Unit Template
+CBU = {
+"Central Border Units-1",
+"Central Border Units-2",
+"Central Border Units-3" }
+
 
 --Spawns
 NorthernSpawnForces = SPAWN:New( "Northern Border Forces" )
@@ -27,4 +51,12 @@ NorthernSpawnForces = SPAWN:New( "Northern Border Forces" )
 :InitRandomizeRoute( 0, 1, 200 )
 :InitRandomizeZones( NorhternSpawnZones )
 :InitRandomizePosition(NBU)
+:SpawnScheduled( 5, .5 )
+
+CentralSpawnForces = SPAWN:New( "Central Border Forces" )
+:InitRandomizeTemplate(CBU)
+:InitLimit( 104, 0 )
+:InitRandomizeRoute( 0, 1, 200 )
+:InitRandomizeZones( CentralSpawnZones )
+:InitRandomizePosition(CBU)
 :SpawnScheduled( 5, .5 )
--- Border Tasking.lua
+++ Border Tasking.lua
@@ -26,14 +26,3 @@ AttackGroups = SET_GROUP:New():FilterCoalitions( "blue" ):FilterStart()
 TargetSetUnit = SET_UNIT:New():FilterCoalitions("red"):FilterPrefixes( "Central" ):FilterStart()
 
 TaskCAS = TASK_A2G_CAS:New( Central_Border_Push, AttackGroups,"Central Border sweep", TargetSetUnit )
-
---Southern Border Task
-Southern_Border_Push = MISSION
-  :New( CommandCenter, "Southern Border Strike", "Primary", "Sweep the boarder and eliminate enenmy forces!", coalition.side.blue )
-  :AddScoring( Scoring )
-
-AttackGroups = SET_GROUP:New():FilterCoalitions( "blue" ):FilterStart()
-
-TargetSetUnit = SET_UNIT:New():FilterCoalitions("red"):FilterPrefixes( "Southern" ):FilterStart()
-
-TaskCAS = TASK_A2G_CAS:New( Southern_Border_Push, AttackGroups,"Southern Border sweep", TargetSetUnit )
