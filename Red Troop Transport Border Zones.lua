9f1d26351c58b3d01a4e57fe03aca5df00d32b26
Grizlog <3rdwood@gmail.com>
2021-10-24 22:05:05 -0500

    added A2A tasking made changes to zeus template removed AI cargo(no worky)

--- Red Troop Transport Border Zones.lua
+++ Red Troop Transport Border Zones.lua
@@ -1,9 +1,10 @@
+--[[
 Reinforcementtemplate={
-  "RedInfantryMortor",
-  "RedInfantryMANPAD",
-  "RedInfantryMortor-2",
-  "RedInfantryMortor-3"
+  "RedInf1",--5 Insurgents AK-74U, 1 RPG, 1 Mortor
+  "RedInf2",--4 Insurgents AK-74U, 1 RPG, 1 MANPAD
+  "RedInf3"--1
 }
+
 APCtemplate={
   "APC Border transport",
   "APC Border transport-1",
@@ -13,28 +14,25 @@ APCtemplate={
 
 SpawnInfantry = SPAWN:New( "RedInfantry1" )
 :InitRandomizeTemplate(Reinforcementtemplate)
-:InitLimit( 30, 100 )
+:InitLimit( 1, 0 )
 :InitRandomizePosition(true,500,100)
-:InitRandomizeZones( { ZONE:New( "Spawn Red Troops-1" ),ZONE:New( "Spawn Red Troops-2" ),ZONE:New( "Spawn Red Troops-3" ),ZONE:New( "Spawn Red Troops-4" ) } )
+:InitRandomizeZones( { ZONE:New( "Spawn Red Troops-1" ),ZONE:New( "Spawn Red Troops-2" ) } )
 :OnSpawnGroup(
+
   function( SpawnGroup )
     CARGO_GROUP:New(SpawnGroup,"InfantryType",SpawnGroup:GetName(),75,10)
   end
 )
-:SpawnScheduled( 2, 0 )
-
-
-local CargoInfantrySet = SET_CARGO:New():FilterTypes( "InfantryType" ):FilterStart()
+--SpawnScheduled( 2, 0 )
+--]]
+--CARGO_GROUP:New(Reinforcementtemplate,"InfantryType",Reinforcementtemplate,75,10)
+local CargoInfantrySet = SET_CARGO:New():FilterTypes( "Infantry" ):FilterStart()
 local SetAPC = SET_GROUP:New():FilterPrefixes( "APC" ):FilterStart()
 local PickupZonesSet = SET_ZONE:New():FilterPrefixes( "Spawn" ):FilterOnce()
 local DeployZonesSet = SET_ZONE:New():FilterPrefixes( "Deploy" ):FilterOnce()
 
 -- For the manpads to unload on time, a range of 8000 meters is appropriate.
-AICargoDispatcherAPC = AI_CARGO_DISPATCHER_APC:New( SetAPC, CargoInfantrySet, nil, DeployZonesSet, 0 )
-AICargoDispatcherAPC:SetPickupOffRoad(false, Formation)
-AICargoDispatcherAPC:SetDeployOffRoad(false, Formation)
-
-AICargoDispatcherAPC = AI_CARGO_DISPATCHER_APC:New( SetAPC, CargoInfantrySet, nil, DeployZonesSet, 0 )
+AICargoDispatcherAPC = AI_CARGO_DISPATCHER_APC:New( SetAPC, CargoInfantrySet, PickupZonesSet, DeployZonesSet, 0 )
 AICargoDispatcherAPC:SetPickupOffRoad(false, Formation)
 AICargoDispatcherAPC:SetDeployOffRoad(false, Formation)
 
@@ -42,7 +40,7 @@ AICargoDispatcherAPC:SetDeployOffRoad(false, Formation)
 --AICargoDispatcherAPC = AI_CARGO_DISPATCHER_APC:New( SetAPC, SetCargoInfantry, nil, SetDeployZones, 0 )
 
 -- Now also make the carriers spawn in.
-CarrierSpawn = SPAWN:New( "APC Trasport-1" )
+--[[CarrierSpawn = SPAWN:New( "APC Trasport-1" )
 :InitRandomizeTemplate(APCtemplate)
 :InitLimit( 6, 10 )
 :InitRandomizePosition( true, 200, 50 )
@@ -65,5 +63,5 @@ CarrierSpawn3 = SPAWN:New( "APC Trasport-4" )
 :InitLimit( 6, 10 )
 :InitRandomizePosition( true, 200, 50 )
 :SpawnScheduled( 10, 0 )
-
+--]]
 AICargoDispatcherAPC:Start()
