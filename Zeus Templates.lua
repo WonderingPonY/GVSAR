0033f0490f4a6ea7375f4269349fe24ab12784e1
C.Erdos <wonderingpony@gmail.com>
2021-10-24 22:00:20 -0600

    added A2A tasking made changes to zeus template removed AI cargo(no worky)

--- Zeus Templates.lua
+++ Zeus Templates.lua
@@ -9,32 +9,32 @@
 "AAAMantis-2" --1 ZU-23 emplacement
 "AAAMantis-3"--1 Shilka
 --Infantry
-"RedInf-1"--5 Insurgents AK-74U, 1 RPG, 1 Mortor
-"RedInf-2"--4 Insurgents AK-74U, 1 RPG, 1 MANPAD
-"RedInf-3"--1 Scout BRDM, 4 Insurgents AK-74U, 2 RPG
+"RedInf1"--5 Insurgents AK-74U, 1 RPG, 1 Mortor
+"RedInf2"--4 Insurgents AK-74U, 1 RPG, 1 MANPAD
+"RedInf3"--1 Scout BRDM, 4 Insurgents AK-74U, 2 RPG
 --Armor
-"RedArm-1"--1 APC BTR-80
-"RedArm-2"--1 IFV BMP-1
-"RedArm-3"--1 IFV BMP-2
-"RedArm-4"--1 IFV BTR-82A
-"RedArm-5"--1 T-55
-"RedArm-6"--1 T-72
-"RedArm-7"--1 Scout BRDM-2
+"APC RedArm1"--1 APC BTR-80
+"RedArm2"--1 IFV BMP-1
+"RedArm3"--1 IFV BMP-2
+"RedArm4"--1 IFV BTR-82A
+"RedArm5"--1 T-55
+"RedArm6"--1 T-72
+"RedArm7"--1 Scout BRDM-2
 --Fortifications
-"RedFort-1"--1 Outpost (Default heading opening on left)
-"RedFort-2"--1 Road Outpost(Default heading road should be on left)
-"RedFort-3"--1 Watch tower
+"RedFort1"--1 Outpost (Default heading opening on left)
+"RedFort2"--1 Road Outpost(Default heading road should be on left)
+"RedFort3"--1 Watch tower
 --unarmed/transports
-"RedTransport-1"--1 LUV Tigr
-"RedTransport-2"--1 Truck GAZ-3308 (supply truck)
-"RedTransport-3"--1 Truck KrAZ-6322 (Supply Truck)
-"RedTransport-4"--1 Truck Ural-375
-"RedTransport-5"--1 ZIL-135 (Big Supply Truck)
+"RedTransport1"--1 LUV Tigr
+"APC RedTransport2"--1 Truck GAZ-3308 (supply truck)
+"RedTransport3"--1 Truck KrAZ-6322 (Supply Truck)
+"RedTransport4"--1 Truck Ural-375
+"RedTransport5"--1 ZIL-135 (Big Supply Truck)
 --Arty
-"RedArty-1"--1 MLRS 9A52 Smerch HE
-"RedArty-2"--1 MLRS 9K57 Uragan
-"RedArty-3"--1 MLRS BM-21 Grad
-"RedArty-4"--1 SPH 2S1 Gvozdika
+"RedArty1"--1 MLRS 9A52 Smerch HE
+"RedArty2"--1 MLRS 9K57 Uragan
+"RedArty3"--1 MLRS BM-21 Grad
+"RedArty4"--1 SPH 2S1 Gvozdika
 
 --Static Objects
 "Red Big smoke"--Big Smoke
