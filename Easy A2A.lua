532d0229b57c75e08ca96b5301499bceb202da88
Grizlog <3rdwood@gmail.com>
2021-10-25 15:34:34 -0500

    Added A2A on demand

--- Easy A2A.lua
+++ Easy A2A.lua
@@ -10,10 +10,9 @@ A2ASpawnsZones = {
 EasyA2A = {
   "EasyAerial1",
   "EasyAerial2"
-
+}
 EasyA2A = SPAWN:New( "easyfighter" )
-:InitRandomizeTemplate(EasyA2A)
+:InitRandomizeTemplate( EasyA2A )
 :InitLimit( 2, 0 )
-:InitRandomizeRoute( 0, 1, 200 )
 :InitRandomizeZones( A2ASpawnsZones )
 :SpawnScheduled( 5, .5 )
