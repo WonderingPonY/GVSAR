7e2dd90d2ebb133e4e6d6bdb5407b2c49f57885d
Grizlog <3rdwood@gmail.com>
2021-10-25 21:18:00 -0500

    added actual task script

--- Easy A2A.lua
+++ Easy A2A.lua
@@ -16,3 +16,78 @@ EasyA2A = SPAWN:New( "easyfighter" )
 :InitLimit( 2, 0 )
 :InitRandomizeZones( A2ASpawnsZones )
 :SpawnScheduled( 5, .5 )
+
+local HQ = GROUP
+  :FindByName( "Bravo HQ" )
+
+local CommandCenter = COMMANDCENTER
+  :New( HQ, "Lima" ) -- Create the CommandCenter.
+
+  EasyA2ATask = MISSION
+    :New( CommandCenter, "Norhtern Border Strike", "Primary", "Sweep the boarder and eliminate enenmy forces!", coalition.side.blue )
+    :AddScoring( Scoring )
+
+function TASK_A2A_INTERCEPT:New( Mission, SetGroup, TaskName, TargetSetUnit, TaskBriefing )
+   local self = BASE:Inherit( self, TASK_A2A:New( Mission, SetGroup, TaskName, TargetSetUnit, "INTERCEPT", TaskBriefing ) ) -- #TASK_A2A_INTERCEPT
+   self:F()
+
+   Mission:AddTask( self )
+
+   self:SetBriefing(
+     TaskBriefing or
+     "Intercept incoming intruders.\n"
+   )
+
+   return self
+ end
+
+ --- Set a score when a target in scope of the A2A attack, has been destroyed .
+ -- @param #TASK_A2A_INTERCEPT self
+ -- @param #string PlayerName The name of the player.
+ -- @param #number Score The score in points to be granted when task process has been achieved.
+ -- @param Wrapper.Unit#UNIT TaskUnit
+ -- @return #TASK_A2A_INTERCEPT
+ function TASK_A2A_INTERCEPT:SetScoreOnProgress( PlayerName, Score, TaskUnit )
+   self:F( { PlayerName, Score, TaskUnit } )
+
+   local ProcessUnit = self:GetUnitProcess( TaskUnit )
+
+   ProcessUnit:AddScoreProcess( "Engaging", "Account", "AccountForPlayer", "Player " .. PlayerName .. " has intercepted a target.", Score )
+
+   return self
+ end
+
+ --- Set a score when all the targets in scope of the A2A attack, have been destroyed.
+ -- @param #TASK_A2A_INTERCEPT self
+ -- @param #string PlayerName The name of the player.
+ -- @param #number Score The score in points.
+ -- @param Wrapper.Unit#UNIT TaskUnit
+ -- @return #TASK_A2A_INTERCEPT
+ function TASK_A2A_INTERCEPT:SetScoreOnSuccess( PlayerName, Score, TaskUnit )
+   self:F( { PlayerName, Score, TaskUnit } )
+
+   local ProcessUnit = self:GetUnitProcess( TaskUnit )
+
+   ProcessUnit:AddScore( "Success", "All targets have been successfully intercepted!", Score )
+
+   return self
+ end
+
+ --- Set a penalty when the A2A attack has failed.
+ -- @param #TASK_A2A_INTERCEPT self
+ -- @param #string PlayerName The name of the player.
+ -- @param #number Penalty The penalty in points, must be a negative value!
+ -- @param Wrapper.Unit#UNIT TaskUnit
+ -- @return #TASK_A2A_INTERCEPT
+ function TASK_A2A_INTERCEPT:SetScoreOnFail( PlayerName, Penalty, TaskUnit )
+   self:F( { PlayerName, Penalty, TaskUnit } )
+
+   local ProcessUnit = self:GetUnitProcess( TaskUnit )
+
+   ProcessUnit:AddScore( "Failed", "The intercept has failed!", Penalty )
+
+   return self
+ end
+
+
+end
