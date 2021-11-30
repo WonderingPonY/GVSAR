0b8ad4b4b2abfef2e7dd72fb816fc1db90f0fe8b
Grizlog <3rdwood@gmail.com>
2021-11-30 16:46:57 -0600

    updated A2G dispatcher script

--- red_a2g.lua
+++ red_a2g.lua
@@ -1,7 +1,7 @@
 --Uses V2 of the AI A2G
 
 local DetectionSetGroup = SET_GROUP:New()
-DetectionSetGroup:FilterPrefixes( { "CCCP Recce" } )
+DetectionSetGroup:FilterPrefixes( { "CCCP" } )
 DetectionSetGroup:FilterStart()
 
 local Detection = DETECTION_AREAS:New( DetectionSetGroup, 8000 )
@@ -15,8 +15,16 @@ local HQ_CC = COMMANDCENTER:New( HQ_Group, "HQ_Red" )
 
 -- Add defense coordinates.
 A2GDispatcher:AddDefenseCoordinate( "HQ_Red", HQ_Group:GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "WF Factory_1", GROUP:FindByName( "WF Factory_1" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Aircarft Factory_1", GROUP:FindByName( "Aircarft Factory_1" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Power Station_1", GROUP:FindByName( "Power Station_1" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "100th Regiment", GROUP:FindByName( "100th Regiment" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Marj Ruhayyil DP", GROUP:FindByName( "Marj Ruhayyil DP" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Khalkhalah Defence-5", GROUP:FindByName( "Khalkhalah Defence-5" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Defence Outpost _1", GROUP:FindByName( "Defence Outpost _1" ):GetCoordinate() )
+A2GDispatcher:AddDefenseCoordinate( "Defence Outpost _2", GROUP:FindByName( "Defence Outpost _2" ):GetCoordinate() )
 A2GDispatcher:SetDefenseReactivityHigh() -- High defense reactivity. So far proximity of a threat will trigger a defense action.
-A2GDispatcher:SetDefenseRadius( 200000 ) -- Defense radius wide enough to also trigger defenses far away.
+A2GDispatcher:SetDefenseRadius( 20921 ) -- Defense radius wide enough to also trigger defenses far away.
 
 -- Communication to the players within the coalition. The HQ services the communication of the defense actions.
 A2GDispatcher:SetCommandCenter( HQ_CC )
@@ -31,7 +39,7 @@ A2GDispatcher:SetTacticalDisplay( false )
 local PatrolZone = ZONE:New( "CAPZONEREDMID" )
 
 -- SEAD from Marj.
-A2GDispatcher:SetSquadron( "Marj SEAD", "Marj Ruhayyil", { "SGroundAttackSu24MRDR" }, 10 )
+A2GDispatcher:SetSquadron( "Marj SEAD", "Marj Ruhayyil", { "CCCP SGroundAttackSu24MRDR" }, 10 )
 A2GDispatcher:SetSquadronSeadPatrol2( "Marj SEAD", PatrolZone, 700, 750, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" ) -- New API
 A2GDispatcher:SetSquadronSeadPatrolInterval( "Marj SEAD", 1, 30, 60, 1 )
 A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Marj SEAD" )
@@ -42,7 +50,7 @@ A2GDispatcher:SetDefaultPatrolLimit("Marj SEAD", 1)
 A2GDispatcher:SetSquadronLanding("Marj SEAD", "Marj Ruhayyil")
 
 -- CAS from Marj
-A2GDispatcher:SetSquadron( "Marj CAS", "Marj Ruhayyil", { "SGroundAttackSu24MGND" }, 10 )
+A2GDispatcher:SetSquadron( "Marj CAS", "Marj Ruhayyil", { "CCCP SGroundAttackSu24MGND" }, 10 )
 A2GDispatcher:SetSquadronCasPatrol2( "Marj CAS", PatrolZone, 700, 750, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" ) -- New API
 A2GDispatcher:SetSquadronCasPatrolInterval( "Marj CAS", 1, 30, 60, 1 )
 A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Marj CAS" )
@@ -53,7 +61,7 @@ A2GDispatcher:SetDefaultPatrolLimit("Marj CAS", 1)
 A2GDispatcher:SetSquadronLanding("Marj CAS", "Marj Ruhayyil")
 
 -- CAS from Khalkhalah
-A2GDispatcher:SetSquadron( "Khalkhalah CAS", "Khalkhalah", { "SCRotary" }, 10 )
+A2GDispatcher:SetSquadron( "Khalkhalah CAS", "Khalkhalah", { "CCCP SCRotary" }, 10 )
 A2GDispatcher:SetSquadronCasPatrol2( "Khalkhalah CAS", PatrolZone, 50, 80, 600, 700, "BARO", 200, 230, 30, 30, "RADIO" ) -- New API
 A2GDispatcher:SetSquadronCasPatrolInterval( "Khalkhalah CAS", 1, 30, 60, 1 )
 A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Khalkhalah CAS" )
@@ -63,7 +71,7 @@ A2GDispatcher:SetDefaultPatrolLimit("Khalkhalah CAS", 1)
 A2GDispatcher:SetSquadronLanding("Khalkhalah CAS", "Khalkhalah")
 
 -- BAI from Khalkhalah
-A2GDispatcher:SetSquadron( "Khalkhalah BAI", "Khalkhalah", { "SCRotary" }, 10 )
+A2GDispatcher:SetSquadron( "Khalkhalah BAI", "Khalkhalah", { "CCCP SCRotary" }, 10 )
 A2GDispatcher:SetSquadronBaiPatrol2( "Khalkhalah BAI", PatrolZone, 50, 80, 600, 700, "BARO", 200, 230, 800, 900, "RADIO" ) -- New API
 A2GDispatcher:SetSquadronBaiPatrolInterval( "Khalkhalah BAI", 1, 30, 60, 1 )
 A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Khalkhalah BAI" )
@@ -78,4 +86,5 @@ A2GDispatcher:SetSquadronTakeoffInterval( "Marj SEAD", 10 )
 A2GDispatcher:SetSquadronTakeoffInterval( "Khalkhalah CAS", 60 )
 A2GDispatcher:SetSquadronTakeoffInterval( "Khalkhalah BAI", 60 )
 A2GDispatcher:SetDefaultDamageThreshold(0.50)
+A2GDispatcher:SetDefaultLanding( AI_A2G_DISPATCHER.Landing.NearAirbase )
 --A2GDispatcher:SetDefaultPatrolLimit(1)
