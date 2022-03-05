32dbd00c2e211c036baa40c4bacd02c16092dfcb
Grizlog <3rdwood@gmail.com>
2022-03-05 12:53:00 -0600

    created Caucasus PVE Mission

---
+++ Caucasus PVE/red_a2a_Cauc.lua
@@ -0,0 +1,71 @@
+DetectionSetGroup = SET_GROUP:New()
+DetectionSetGroup:FilterPrefixes( { "CaucEWR" } )
+DetectionSetGroup:FilterStart()
+
+-- Setup the detection and group targets to a 30km range!
+Detection = DETECTION_AREAS:New( DetectionSetGroup, 300000 )
+Detection:SetRefreshTimeInterval( 10 )
+--Filter Catagories
+DetectionSetGroup:FilterCategories( Unit.Category.Airplane )
+
+-- Setup the A2A dispatcher, and initialize it.
+A2ADispatcher = AI_A2A_DISPATCHER:New( Detection )
+A2ADispatcher:SetTacticalDisplay( false )
+A2ADispatcher:SetRefreshTimeInterval( 10 )
+
+
+A2ADispatcher:SetEngageRadius( 120000 )
+--BorderZone
+BorderZone = ZONE_POLYGON:New( "Cuacborder", GROUP:FindByName( "Cuacborder" ) )
+A2ADispatcher:SetBorderZone( BorderZone )
+local GudPatrolZone = ZONE:New("CapZoneGud")
+local SenakiPatrolZone = ZONE:New("CapZoneSenaki")
+local TbilisiPatrolZone = ZONE:New("CapZoneTbilisi")
+local EastRusPatrolZone = ZONE:New("CapZoneRusEast")
+local WestRusPatrolZone = ZONE:New("CapZoneRusWest")
+
+--Here we will make the squadrons and set options for each squadron
+-- Squadron 1
+A2ADispatcher:SetSquadron( "Sq 1", AIRBASE.Caucasus.Vaziani, { "SFighterMiG29" }, 15 )
+A2ADispatcher:SetSquadronCap( "Sq 1", TbilisiPatrolZone, 4000, 8000, 600, 800, 800, 1200, "BARO" )
+A2ADispatcher:SetSquadronGci( "Sq 1", 1000, 1500 )
+A2ADispatcher:SetSquadronLandingNearAirbase( "Sq 1" , "Vaziani")
+A2ADispatcher:SetSquadronGrouping( "Sq 1", 2 )
+A2ADispatcher:SetSquadronOverhead( "Sq 1", 0.1)
+A2ADispatcher:SetSquadronFuelThreshold( "Sq 1", 0.35 )
+A2ADispatcher:SetSquadronCapInterval( "Sq 1", 1, 600, 1200, 1 )
+--A2ADispatcher:SetSquadronLanding( "Sq 1" , "Palmyra")
+--Squadron 2
+A2ADispatcher:SetSquadron( "Sq 2", AIRBASE.Caucasus.Sochi, { "SPatrolMiG21" }, 40 )
+A2ADispatcher:SetSquadronCap( "Sq 2", SenakiPatrolZone, 4000, 8000, 600, 800, 800, 1200, "BARO" )
+A2ADispatcher:SetSquadronGci( "Sq 2", 500, 1500 )
+A2ADispatcher:SetSquadronLandingNearAirbase( "Sq 2" , "Sochi-Adler")
+A2ADispatcher:SetSquadronGrouping( "Sq 2", 2 )
+A2ADispatcher:SetSquadronOverhead( "Sq 2", 0.1)
+A2ADispatcher:SetSquadronFuelThreshold( "Sq 2", 0.25 )
+A2ADispatcher:SetSquadronCapInterval( "Sq 2", 1, 600, 1200, 1 )
+--A2ADispatcher:SetSquadronLanding( "Sq 2" , "Palmyra")
+--Squadron 3
+A2ADispatcher:SetSquadron( "Sq 3", AIRBASE.Caucasus.Sochi, { "GPatrolMiG21" }, 40 )
+A2ADispatcher:SetSquadronCap( "Sq 3", GudPatrolZone, 4000, 8000, 600, 800, 800, 1200, "BARO" )
+A2ADispatcher:SetSquadronGci( "SSq 3", 500, 1500 )
+A2ADispatcher:SetSquadronLandingNearAirbase( "Sq 3" , "Sochi-Adler")
+A2ADispatcher:SetSquadronGrouping( "Sq 3", 2 )
+A2ADispatcher:SetSquadronOverhead( "Sq 3", 0.1)
+A2ADispatcher:SetSquadronFuelThreshold( "Sq 3", 0.25 )
+A2ADispatcher:SetSquadronCapInterval( "Sq 3", 1, 600, 1200, 1 )
+--A2ADispatcher:SetSquadronLanding( "Sq 2" , "Palmyra")
+--Squadron 4
+-- A2ADispatcher:SetSquadron( "Sq 4", "Sayqal", { "SPatrolMiG21" }, 40 )
+-- A2ADispatcher:SetSquadronCap( "Sq 4", CAPZoneMid, 4000, 8000, 600, 800, 800, 1200, "BARO" )
+-- A2ADispatcher:SetSquadronGci( "Sq 4", 500, 1500 )
+-- A2ADispatcher:SetSquadronLandingNearAirbase( "Sq 4" , "Sayqal")
+-- A2ADispatcher:SetSquadronGrouping( "Sq 4", 2 )
+-- A2ADispatcher:SetSquadronOverhead( "Sq 4", 0.1)
+-- A2ADispatcher:SetSquadronFuelThreshold( "Sq 4", 0.25 )
+-- A2ADispatcher:SetSquadronCapInterval( "Sq 4", 1, 600, 1200, 1 )
+--A2ADispatcher:SetSquadronLanding( "Sq 4" , "Sayqal")
+
+A2ADispatcher:SetDefaultCapLimit(2)
+--A2ADispatcher:SetDefaultTakeOffFromParkingHot()
+--A2ADispatcher:SetDefaultLandingNearAirbase()
---
+++ Caucasus PVE/red_a2g_Cauc.lua
@@ -0,0 +1,72 @@
+-- Define a SET_GROUP object that builds a collection of groups that define the recce network.
+-- Here we build the network with all the groups that have a name starting with CCCP Recce.
+local DetectionSetGroup = SET_GROUP:New():FilterStart()
+DetectionSetGroup:FilterPrefixes( "CCCP" )
+
+local Detection = DETECTION_AREAS:New( DetectionSetGroup, 5000 )
+
+-- Setup the A2A dispatcher, and initialize it.
+A2GDispatcher = AI_A2G_DISPATCHER:New( Detection )
+
+local HQ_Group = GROUP:FindByName( "HQ" )
+local HQ_CC = COMMANDCENTER:New( HQ_Group, "HQ" )
+
+-- Add defense coordinates.
+A2GDispatcher:AddDefenseCoordinate( "Dicktator", GROUP:FindByName( "Dicktator" ):GetCoordinate() )
+-- A2GDispatcher:AddDefenseCoordinate( "DomComm", GROUP:FindByName( "DomComm" ):GetCoordinate() )
+
+A2GDispatcher:SetDefenseReactivityHigh()
+
+A2GDispatcher:SetDefenseRadius( 370400 )
+
+A2GDispatcher:SetCommandCenter( HQ_CC )
+
+A2GDispatcher:SetTacticalDisplay( false )
+DetectionSetGroup:Count()
+BASE:E("Group set count " .. DetectionSetGroup:Count())
+
+local GudPatrolZone = ZONE:New("CapZoneGud")
+local SenakiPatrolZone = ZONE:New("CapZoneSenaki")
+local TbilisiPatrolZone = ZONE:New("CapZoneTbilisi")
+local EastRusPatrolZone = ZONE:New("CapZoneRusEast")
+local WestRusPatrolZone = ZONE:New("CapZoneRusWest")
+
+
+-- Setup the squadrons.
+
+A2GDispatcher:SetSquadron( "Sochi SEAD", AIRBASE.Caucasus.Sochi, { "CCCP SGroundAttackSu24MRDR" }, 10 )
+A2GDispatcher:SetSquadronSeadPatrol2( "Sochi SEAD", SenakiPatrolZone, 500, 550, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" )
+A2GDispatcher:SetSquadronSeadPatrolInterval( "Sochi SEAD", 2, 300, 600, 1 )
+A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Sochi SEAD" )
+A2GDispatcher:SetSquadronOverhead( "Sochi SEAD", 0.2 )
+
+A2GDispatcher:SetSquadron( "Sochi BAI", AIRBASE.Caucasus.Sochi, { "CCCP SGroundAttackSu24MGND" }, 10 )
+A2GDispatcher:SetSquadronBaiPatrol2( "Sochi BAI", SenakiPatrolZone, 500, 550, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" )
+A2GDispatcher:SetSquadronBaiPatrolInterval( "Sochi BAI", 2, 300, 600, 1 )
+A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Sochi BAI" )
+A2GDispatcher:SetSquadronOverhead( "Sochi BAI", 0.25 )
+
+A2GDispatcher:SetSquadron( "Senaki BAI", AIRBASE.Caucasus.Senaki, { "CCCP SenakiRotary" }, 10 )
+A2GDispatcher:SetSquadronBaiPatrol2( "Senaki BAI", SenakiPatrolZone, 500, 550, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" )
+A2GDispatcher:SetSquadronBaiPatrolInterval( "Senaki BAI", 2, 300, 600, 1 )
+A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Senaki BAI" )
+A2GDispatcher:SetSquadronOverhead( "Senaki BAI", 0.25 )
+
+A2GDispatcher:SetSquadron( "Kutaisi BAI", AIRBASE.Caucasus.Kutaisi, { "CCCP KutRotary" }, 10 )
+A2GDispatcher:SetSquadronBaiPatrol2( "Kutaisi BAI", SenakiPatrolZone, 500, 550, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" )
+A2GDispatcher:SetSquadronBaiPatrolInterval( "Kutaisi BAI", 2, 300, 600, 1 )
+A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Kutaisi BAI" )
+A2GDispatcher:SetSquadronOverhead( "Kutaisi BAI", 0.25 )
+
+-- A2GDispatcher:SetSquadron( "Qabr BAI", AIRBASE.Syria.Qabr_as_Sitt, { "CCCP SCRotary" }, 10 )
+-- A2GDispatcher:SetSquadronBaiPatrol2( "Qabr BAI", PatrolZone, 500, 550, 2000, 2000, "BARO", 750, 800, 30, 30, "RADIO" )
+-- A2GDispatcher:SetSquadronBaiPatrolInterval( "Qabr BAI", 2, 30, 60, 1 )
+-- A2GDispatcher:SetSquadronTakeoffFromParkingHot( "Qabr BAI" )
+-- A2GDispatcher:SetSquadronOverhead( "Qabr BAI", 0.25 )
+
+--Takeoff Itnervals
+A2GDispatcher:SetSquadronTakeoffInterval( "Sochi SEAD", 60 )
+A2GDispatcher:SetSquadronTakeoffInterval( "Sochi BAI", 60 )
+A2GDispatcher:SetSquadronTakeoffInterval( "Senaki BAI", 60 )
+A2GDispatcher:SetSquadronTakeoffInterval( "Kutaisi BAI", 60 )
+-- A2GDispatcher:SetSquadronTakeoffInterval( "Qabr BAI", 60 )
