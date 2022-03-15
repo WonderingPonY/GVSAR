6341068043a8945669c5026ad23a0c74425bb950
Grizlog <3rdwood@gmail.com>
2022-03-15 17:38:38 -0500

    added tanker script

---
+++ Caucasus PVE/Cauctankers.lua
@@ -0,0 +1,23 @@
+--///////////////////////////////////////////////////
+--Tankers Spawn
+--///////////////////////////////////////////////////
+
+-- local Shell_KC130 = SPAWN:New( "Tanker 135" )
+--  Shell_KC130:InitLimit(1,100)
+--  Shell_KC130:InitRepeatOnLanding()
+--  Shell_KC130:SpawnScheduled(30,0)
+
+-- local Shell_KC130MPRS = SPAWN:New( "Tanker 135MPRS" )
+-- Shell_KC130MPRS:InitLimit(1,100)
+-- Shell_KC130MPRS:InitRepeatOnLanding()
+-- Shell_KC130MPRS:SpawnScheduled(30,0)
+
+-- local Shell_SKC130 = SPAWN:New( "Tanker 135A10" )
+-- Shell_SKC130:InitLimit(1,100)
+-- Shell_SKC130:InitRepeatOnLanding()
+-- Shell_SKC130:SpawnScheduled(30,0)
+
+local AWACS_E3A = SPAWN:New( "Big Daddy" )
+AWACS_E3A:InitLimit(1,100)
+AWACS_E3A:InitRepeatOnLanding()
+AWACS_E3A:SpawnScheduled(30,0)
