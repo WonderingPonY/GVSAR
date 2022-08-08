b3b1bc5edf3cc03182551dfee510947b225c8c25
Grizlog <3rdwood@gmail.com>
2022-08-08 18:00:06 -0500

    added updated persistant files to mission

--- Caucasus/HeliDomination/Domination Persistent World.lua
+++ Caucasus/HeliDomination/Domination Persistent World.lua
@@ -63,7 +63,7 @@ function file_exists(name) --check if the file already exists for writing
 end
 
 function writemission(data, file)--Function for saving to file (commonly found)
-	File = io.open(("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\"..file), "w")
+	File = io.open(("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\"..file), "w")
 	File:write(data)
 	File:close()
 end
@@ -100,93 +100,93 @@ function SEF_SaveStaticIntermentTableNoArgs()
 	writemission(IntermentMissionStrStatic, "SeaSlugStaticInterment.lua")
 end
 
-function SEF_SaveAirbasesTable(timeloop, time)
-	SEF_PERSISTENTAIRBASES(PersistentAirbases)
-	AirbaseStr = IntegratedserializeWithCycles("SeaSlugAirbases", SeaSlugAirbases)
-	writemission(AirbaseStr, "SeaSlugAirbases.lua")
-	return time + SaveScheduleUnits
-end
-
-function SEF_SaveAirbasesTableNoArgs()
-	SEF_PERSISTENTAIRBASES(PersistentAirbases)
-	AirbaseStr = IntegratedserializeWithCycles("SeaSlugAirbases", SeaSlugAirbases)
-	writemission(AirbaseStr, "SeaSlugAirbases.lua")
-end
-
-PersistentAirbases = {
-
-	"Gelendzik",
-  "Novorossiysk",
-  "Anapa-Vityazevo",
-  "Krymsk",
-  "Krasnodar-Center",
-  "Krasnodar-Pashkovsky",
-  "Maykop-Khanskaya",
-  "Mineralnye Vody",
-  "Nalchik",
-  "Mozdok",
-  "Beslan",
-  "Tiblisi-Lochini",
-  "Vaziani",
-  "Soganlug",
-  "Kutaisi",
-  "Senaki-Kolkhi",
-  "Kobuleti",
-  "Batumi",
-  "Sukhumi-Babushara",
-  "Gudauta",
-  "Sochi-Adler"
-
-}
-
-REDCAPTURETEAM 	= SPAWN:New("REDCAP")
-BLUECAPTURETEAM = SPAWN:New("BLUECAP")
-
-function SEF_PERSISTENTAIRBASES(AirbaseList)
-	SeaSlugAirbases = {}
-
-	for i, ab in ipairs(AirbaseList) do
-		local AirbaseObject = Airbase.getByName(ab)
-		local AirbaseCoalition = AirbaseObject:getCoalition()
-
-		TempAirbaseTable = {
-			["Airbase"]=ab,
-			["Coalition"]=AirbaseCoalition
-		}
-		table.insert(SeaSlugAirbases, TempAirbaseTable )
-	end
-end
-
-function SEF_CAPAIRBASE(Airbase, Coalition)
-
-	if ( Coalition == 1 ) then
-    trigger.action.outText(Airbase, 15)
-		RedHeloSpawnVec2 = ZONE:FindByName(Airbase):GetVec2()
-
-		--////CREATE OFFSETS
-		local OffsetX = math.random(-40, 40)
-		local OffsetY = math.random(-40, 40)
-		local SpawnX = RedHeloSpawnVec2.x + OffsetX
-		local SpawnY = RedHeloSpawnVec2.y + OffsetY
-		local OffsetSpawnVec2 = { x = SpawnX, y = SpawnY }
-
-		REDCAPTURETEAM:SpawnFromVec2(OffsetSpawnVec2)
-    trigger.action.outText("Spawn RED", 15)
-	elseif ( Coalition == 2 ) then
-		BlueHeloSpawnVec2 = ZONE:FindByName(Airbase):GetVec2()
-
-		--////CREATE OFFSETS
-		local OffsetX = math.random(-40, 40)
-		local OffsetY = math.random(-40, 40)
-		local SpawnX = BlueHeloSpawnVec2.x + OffsetX
-		local SpawnY = BlueHeloSpawnVec2.y + OffsetY
-		local OffsetSpawnVec2 = { x = SpawnX, y = SpawnY }
-
-		BLUECAPTURETEAM:SpawnFromVec2(OffsetSpawnVec2)
-    trigger.action.outText("Spawn BLUE"..Coalition, 15)
-	else
-	end
-end
+-- function SEF_SaveAirbasesTable(timeloop, time)
+-- 	SEF_PERSISTENTAIRBASES(PersistentAirbases)
+-- 	AirbaseStr = IntegratedserializeWithCycles("SeaSlugAirbases", SeaSlugAirbases)
+-- 	writemission(AirbaseStr, "SeaSlugAirbases.lua")
+-- 	return time + SaveScheduleUnits
+-- end
+--
+-- function SEF_SaveAirbasesTableNoArgs()
+-- 	SEF_PERSISTENTAIRBASES(PersistentAirbases)
+-- 	AirbaseStr = IntegratedserializeWithCycles("SeaSlugAirbases", SeaSlugAirbases)
+-- 	writemission(AirbaseStr, "SeaSlugAirbases.lua")
+-- end
+--
+-- PersistentAirbases = {
+--
+-- 	"Gelendzik",
+--   "Novorossiysk",
+--   "Anapa-Vityazevo",
+--   "Krymsk",
+--   "Krasnodar-Center",
+--   "Krasnodar-Pashkovsky",
+--   "Maykop-Khanskaya",
+--   "Mineralnye Vody",
+--   "Nalchik",
+--   "Mozdok",
+--   "Beslan",
+--   "Tiblisi-Lochini",
+--   "Vaziani",
+--   "Soganlug",
+--   "Kutaisi",
+--   "Senaki-Kolkhi",
+--   "Kobuleti",
+--   "Batumi",
+--   "Sukhumi-Babushara",
+--   "Gudauta",
+--   "Sochi-Adler"
+--
+-- }
+--
+-- REDCAPTURETEAM 	= SPAWN:New("REDCAP")
+-- BLUECAPTURETEAM = SPAWN:New("BLUECAP")
+--
+-- function SEF_PERSISTENTAIRBASES(AirbaseList)
+-- 	SeaSlugAirbases = {}
+--
+-- 	for i, ab in ipairs(AirbaseList) do
+-- 		local AirbaseObject = Airbase.getByName(ab)
+-- 		local AirbaseCoalition = AirbaseObject:getCoalition()
+--
+-- 		TempAirbaseTable = {
+-- 			["Airbase"]=ab,
+-- 			["Coalition"]=AirbaseCoalition
+-- 		}
+-- 		table.insert(SeaSlugAirbases, TempAirbaseTable )
+-- 	end
+-- end
+--
+-- function SEF_CAPAIRBASE(Airbase, Coalition)
+--
+-- 	if ( Coalition == 1 ) then
+--     trigger.action.outText(Airbase, 15)
+-- 		RedHeloSpawnVec2 = ZONE:FindByName(Airbase):GetVec2()
+--
+-- 		--////CREATE OFFSETS
+-- 		local OffsetX = math.random(-40, 40)
+-- 		local OffsetY = math.random(-40, 40)
+-- 		local SpawnX = RedHeloSpawnVec2.x + OffsetX
+-- 		local SpawnY = RedHeloSpawnVec2.y + OffsetY
+-- 		local OffsetSpawnVec2 = { x = SpawnX, y = SpawnY }
+--
+-- 		REDCAPTURETEAM:SpawnFromVec2(OffsetSpawnVec2)
+--     trigger.action.outText("Spawn RED", 15)
+-- 	elseif ( Coalition == 2 ) then
+-- 		BlueHeloSpawnVec2 = ZONE:FindByName(Airbase):GetVec2()
+--
+-- 		--////CREATE OFFSETS
+-- 		local OffsetX = math.random(-40, 40)
+-- 		local OffsetY = math.random(-40, 40)
+-- 		local SpawnX = BlueHeloSpawnVec2.x + OffsetX
+-- 		local SpawnY = BlueHeloSpawnVec2.y + OffsetY
+-- 		local OffsetSpawnVec2 = { x = SpawnX, y = SpawnY }
+--
+-- 		BLUECAPTURETEAM:SpawnFromVec2(OffsetSpawnVec2)
+--     trigger.action.outText("Spawn BLUE"..Coalition, 15)
+-- 	else
+-- 	end
+-- end
 
 -------------------------------------------------------------------------------------------------------------------------------------
 --////MAIN
@@ -195,9 +195,9 @@ SEFDeletedUnitCount = 0
 SEFDeletedStaticCount = 0
 
 --////LOAD UNITS
-if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugUnitInterment.lua") then
+if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugUnitInterment.lua") then
 
-	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugUnitInterment.lua")
+	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugUnitInterment.lua")
 
 	UnitIntermentTableLength = SEF_GetTableLength(SeaSlugUnitInterment)
 	--trigger.action.outText("Unit Table Length Is "..UnitIntermentTableLength, 15)
@@ -217,9 +217,9 @@ else
 	UnitIntermentTableLength = 0
 end
 --////Static units
-if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugStaticInterment.lua") then
+if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugStaticInterment.lua") then
 
-	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugStaticInterment.lua")
+	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugStaticInterment.lua")
 
 	UnitIntermentTableLength = SEF_GetTableLength(SeaSlugStaticInterment)
 	--trigger.action.outText("Unit Table Length Is "..UnitIntermentTableLength, 15)
@@ -240,9 +240,9 @@ else
 end
 
 --////LOAD STATICS
-if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugStaticInterment.lua") then
+if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugStaticInterment.lua") then
 
-	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugStaticInterment.lua")
+	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugStaticInterment.lua")
 
 	StaticIntermentTableLength = SEF_GetTableLength(SeaSlugStaticInterment)
 	--trigger.action.outText("Static Table Length Is "..StaticIntermentTableLength, 15)
@@ -265,44 +265,44 @@ end
 --trigger.action.outText("Persistent World Functions Have Removed "..SEFDeletedUnitCount.." Units and "..SEFDeletedStaticCount.." Static Objects", 15)
 
 --////LOAD AIRBASES
-if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugAirbases.lua") then
-
-	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.test_server\\Persistent Files\\SeaSlugAirbases.lua")
-
-	AirbaseTableLength = SEF_GetTableLength(SeaSlugAirbases)
-
-	for i = 1, AirbaseTableLength do
-		BaseName = SeaSlugAirbases[i].Airbase
-		BaseCoalition = SeaSlugAirbases[i].Coalition
-
-		if ( BaseCoalition == 1) then
-			SEF_CAPAIRBASE(BaseName, 1)
-		elseif ( BaseCoalition == 2 ) then
-			SEF_CAPAIRBASE(BaseName, 2)
-
-			--[[
-			--////Deal With Particular Airbase Late Activations
-			if ( BaseName == "Whatever" ) then
-				--trigger.action.activateGroup(Group.getByName("Whatever"))
-			else
-			end
-			if ( BaseName == "Whatever" ) then
-				--trigger.action.activateGroup(Group.getByName("Whatever"))
-			else
-			end
-			if ( BaseName == "Whatever" ) then
-				--trigger.action.activateGroup(Group.getByName("Whatever"))
-			else
-			end
-			]]--
-
-		else
-		end
-	end
-else
-	SeaSlugAirbases = {}
-	AirbaseTableLength = 0
-end
+-- if file_exists("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugAirbases.lua") then
+--
+-- 	dofile("C:\\Users\\Gracey's Village DCS\\Saved Games\\DCS.domination\\Persistent Files\\SeaSlugAirbases.lua")
+--
+-- 	AirbaseTableLength = SEF_GetTableLength(SeaSlugAirbases)
+--
+-- 	for i = 1, AirbaseTableLength do
+-- 		BaseName = SeaSlugAirbases[i].Airbase
+-- 		BaseCoalition = SeaSlugAirbases[i].Coalition
+--
+-- 		if ( BaseCoalition == 1) then
+-- 			SEF_CAPAIRBASE(BaseName, 1)
+-- 		elseif ( BaseCoalition == 2 ) then
+-- 			SEF_CAPAIRBASE(BaseName, 2)
+--
+-- 			--[[
+-- 			--////Deal With Particular Airbase Late Activations
+-- 			if ( BaseName == "Whatever" ) then
+-- 				--trigger.action.activateGroup(Group.getByName("Whatever"))
+-- 			else
+-- 			end
+-- 			if ( BaseName == "Whatever" ) then
+-- 				--trigger.action.activateGroup(Group.getByName("Whatever"))
+-- 			else
+-- 			end
+-- 			if ( BaseName == "Whatever" ) then
+-- 				--trigger.action.activateGroup(Group.getByName("Whatever"))
+-- 			else
+-- 			end
+-- 			]]--
+--
+-- 		else
+-- 		end
+-- 	end
+-- else
+-- 	SeaSlugAirbases = {}
+-- 	AirbaseTableLength = 0
+-- end
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -310,6 +310,6 @@ end
 --trigger.action.outText("Persistent World Functions Schedulers Are Currently Disabled", 15)
 timer.scheduleFunction(SEF_SaveUnitIntermentTable, 53, timer.getTime() + SaveScheduleUnits)
 timer.scheduleFunction(SEF_SaveStaticIntermentTable, 53, timer.getTime() + (SaveScheduleUnits + 3))
-timer.scheduleFunction(SEF_SaveAirbasesTable, 53, timer.getTime() + (SaveScheduleUnits + 5))
+--timer.scheduleFunction(SEF_SaveAirbasesTable, 53, timer.getTime() + (SaveScheduleUnits + 5))
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------
