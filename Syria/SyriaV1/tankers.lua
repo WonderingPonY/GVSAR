10c5cb0c7f7260b87083bc27b917ab5423dfd563
Grizlog <3rdwood@gmail.com>
2023-06-23 14:39:49 -0500

    Fixing Training Mission

--- Syria/SyriaV1/tankers.lua
+++ Syria/SyriaV1/tankers.lua
@@ -2,17 +2,18 @@
 --Tankers Spawn
 --///////////////////////////////////////////////////
 
-local Shell_KC130 = SPAWN:New( "PipeMan 1-1" )
+
+local Shell_KC130 = SPAWN:New( "Tanker 135" )
  Shell_KC130:InitLimit(1,100)
  Shell_KC130:InitRepeatOnLanding()
  Shell_KC130:SpawnScheduled(30,0)
 
-local Shell_KC130MPRS = SPAWN:New( "Bagman 1-1" )
+local Shell_KC130MPRS = SPAWN:New( "Tanker 135MPRS" )
 Shell_KC130MPRS:InitLimit(1,100)
 Shell_KC130MPRS:InitRepeatOnLanding()
 Shell_KC130MPRS:SpawnScheduled(30,0)
 
-local Shell_SKC130 = SPAWN:New( "Slowboi4A10 1-1" )
+local Shell_SKC130 = SPAWN:New( "Tanker 135A10" )
 Shell_SKC130:InitLimit(1,100)
 Shell_SKC130:InitRepeatOnLanding()
 Shell_SKC130:SpawnScheduled(30,0)
