5c2122ea7f3d0fade132b42b0680b8557d460e72
Grizlog <3rdwood@gmail.com>
2023-07-16 16:13:40 -0500

    fixed CSAR on training Server

--- Caucasus/TrainingMiz/CSAR.lua
+++ Caucasus/TrainingMiz/CSAR.lua
@@ -7,28 +7,45 @@ _SETTINGS:SetImperial()
 my_scoring = SCORING:New("CSAR")
 
 local BlueCsar = CSAR:New(coalition.side.BLUE,"Victim","Luftrettung")
+BlueCsar.invisiblecrew = false
+BlueCsar.mashprefix = {"Hospital"}
+BlueCsar.useprefix = true  -- Requires CSAR helicopter #GROUP names to have the prefix(es) defined below.
+BlueCsar.csarPrefix = { "TRAINING MEDEVAC"}
 BlueCsar.coordtype = 2
+BlueCsar.autosmoke = true -- automatically smoke a downed pilot\'s location when a heli is near.
+BlueCsar.autosmokedistance = 2000
+BlueCsar.smokecolor = 3
+BlueCsar:SpawnCSARAtZone( "HeartAttack", coalition.side.BLUE,"Heart Attack Victim", true )
+BlueCsar:SpawnCSARAtZone( "HikingVictim", coalition.side.BLUE,"Lost Hiker", true )
+BlueCsar:SpawnCSARAtZone( "HikingVictim2", coalition.side.BLUE,"Fall Victim", true )
+BlueCsar:SpawnCSARAtZone( "FarmingVictim", coalition.side.BLUE,"Farming Accident Victim", true )
+BlueCsar:SpawnCSARAtZone( "FarmingVictim2", coalition.side.BLUE,"Farming Accident Victim", true )
+BlueCsar:SpawnCSARAtZone( "BoatingVictim", coalition.side.BLUE,"Drown Victim", true )
+BlueCsar:SpawnCSARAtZone( "HeartAttack2", coalition.side.BLUE,"Fall Victim", true )
+BlueCsar:SpawnCSARAtZone( "BoatingVictim2", coalition.side.BLUE,"Boating Accident", true )
+BlueCsar:SpawnCSARAtZone( "NovoVictim", coalition.side.BLUE,"Heart Attack Victim", true )
+BlueCsar:SpawnCSARAtZone( "SportVictim", coalition.side.BLUE,"Heart Attack Victim", true )
 BlueCsar:__Start(5)
 
-function BlueCsar:OnAfterRescued(From, Event, To, HeliUnit, HeliName, NumberSaved)
-  -- add score to player
-  local NumberSaved = NumberSaved or 1
-  local points = 100 * NumberSaved
-  local PlayerName = HeliUnit:GetPlayerName()
-  my_scoring:_AddPlayerFromUnit( HeliUnit )
-  my_scoring:AddGoalScore(HeliUnit, "CSAR", string.format(" %s has been awarded %d points!", HeliName, points), points)
-end
+-- function BlueCsar:OnAfterRescued(From, Event, To, HeliUnit, HeliName, NumberSaved)
+--   -- add score to player
+--   local NumberSaved = NumberSaved or 1
+--   local points = 100 * NumberSaved
+--   local PlayerName = HeliUnit:GetPlayerName()
+--   my_scoring:_AddPlayerFromUnit( HeliUnit )
+--   my_scoring:AddGoalScore(HeliUnit, "CSAR", string.format(" %s has been awarded %d points!", HeliName, points), points)
+-- end
 
-function Spawn_CSAR(BlueCSAR)
-  BlueCSAR:SpawnCSARAtZone( "HeartAttack", coalition.side.BLUE,"Heart Attack Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "HikingVictim", coalition.side.BLUE,"Lost Hiker", true )
-  BlueCSAR:SpawnCSARAtZone( "HikingVictim2", coalition.side.BLUE,"Fall Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "FarmingVictim", coalition.side.BLUE,"Farming Accident Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "FarmingVictim2", coalition.side.BLUE,"Farming Accident Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "BoatingVictim", coalition.side.BLUE,"Drown Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "HeartAttack2", coalition.side.BLUE,"Fall Victim", true )
-  BlueCSAR:SpawnCSARAtZone( "BoatingVictim2", coalition.side.BLUE,"Boating Accident", true )
-end
-
-local maulwuerfe = TIMER:New(Spawn_CSAR,BlueCsar)
-maulwuerfe:Start(5)
+-- function Spawn_CSAR(BlueCSAR)
+--   BlueCsar:SpawnCSARAtZone( "HeartAttack", coalition.side.BLUE,"Heart Attack Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "HikingVictim", coalition.side.BLUE,"Lost Hiker", true )
+--   BlueCsar:SpawnCSARAtZone( "HikingVictim2", coalition.side.BLUE,"Fall Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "FarmingVictim", coalition.side.BLUE,"Farming Accident Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "FarmingVictim2", coalition.side.BLUE,"Farming Accident Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "BoatingVictim", coalition.side.BLUE,"Drown Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "HeartAttack2", coalition.side.BLUE,"Fall Victim", true )
+--   BlueCsar:SpawnCSARAtZone( "BoatingVictim2", coalition.side.BLUE,"Boating Accident", true )
+-- end
+--
+-- --local maulwuerfe = TIMER:New(Spawn_CSAR,BlueCsar)
+-- BlueCSAR:__Start(5)
